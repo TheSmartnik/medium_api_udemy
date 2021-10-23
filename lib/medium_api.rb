fb8cfadda4e50c8b5fcff11f042d433ce0f6b441
 TheSmartnik <misharinn@gmail.com>
2021-10-23 04:16:37 +0300

    Init commit

---
+++ lib/medium_api.rb
@@ -0,0 +1,6 @@
+require "medium_api/version"
+
+module MediumApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/medium_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MediumApi do
+  it "has a version number" do
+    expect(MediumApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
